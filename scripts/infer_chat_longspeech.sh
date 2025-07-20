eea04af5b157e290d30f5175b1bbac3c632d605d
ShoutaoGuo <108777872+ShoutaoGuo@users.noreply.github.com>
2025-07-20 15:02:01 +0800

    Update and rename infer_chat.sh to infer_chat_longspeech.sh

---
+++ scripts/infer_chat_longspeech.sh
@@ -0,0 +1,11 @@
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH=/data/guoshoutao/LongSpeechLLMs/FastLongSpeech
+
+MODEL=model_dir
+LORA=lora_dir
+LEN=400
+AUDIO_CONFIG=audio_config
+AUDIO_ROOT=audio_root
+TARGET=target
+
+python inference_file/chat_infer_long_speech.py --predefined_length $LEN --model_name $MODEL --lora_model_path $LORA --audio_config $AUDIO_CONFIG --audio_path_root $AUDIO_ROOT --target_path $TARGET
